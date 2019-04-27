be74d223109abb0f02ce39dab5fcc9e21e9e8051
Rasmus Eskola <fruitiex@gmail.com>
2019-04-27 16:08:29 +0300

    Make the SongList virtualized for better performance

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -3,24 +3,58 @@ import { List, Card, ListSubheader } from "@material-ui/core"
 import { Song } from "../types/song"
 import SongListItem from "./SongListItem"
 
+import { FixedSizeList } from "react-window"
+import { WindowScroller } from "react-virtualized"
+
 interface SongListProps {
   songs: Song[]
   bookId: string
   bookTitle: string
 }
 
+const songListItemHeight = 60
+
 const SongList: React.FunctionComponent<SongListProps> = ({
   songs,
   bookId,
   bookTitle,
-}) => (
-  <Card>
-    <List subheader={<ListSubheader>Songs</ListSubheader>} component="nav">
-      {songs.map(song => (
-        <SongListItem bookId={bookId} bookTitle={bookTitle} {...song} />
-      ))}
-    </List>
-  </Card>
-)
+}) => {
+  const listRef = React.useRef<FixedSizeList>(null)
+  const handleScroll = React.useCallback(
+    ({ scrollTop }: { scrollTop: number }) => {
+      if (listRef.current) {
+        listRef.current.scrollTo(scrollTop)
+      }
+    },
+    []
+  )
+
+  // Beware of hacks below to get WindowScroller working with react-window:
+  // https://github.com/bvaughn/react-window/issues/30#issuecomment-428868071
+  return (
+    <Card>
+      <List subheader={<ListSubheader>Songs</ListSubheader>} component="nav">
+        <WindowScroller onScroll={handleScroll}>{() => <div />}</WindowScroller>
+        <FixedSizeList
+          ref={listRef}
+          height={window.innerHeight}
+          width="100%"
+          itemCount={songs.length}
+          itemSize={songListItemHeight}
+          style={{ height: "100%" }}
+        >
+          {({ style, index }) => (
+            <SongListItem
+              song={songs[index]}
+              bookId={bookId}
+              bookTitle={bookTitle}
+              style={style}
+            />
+          )}
+        </FixedSizeList>
+      </List>
+    </Card>
+  )
+}
 
 export default SongList
--- src/components/SongListItem.tsx
+++ src/components/SongListItem.tsx
@@ -4,25 +4,26 @@ import { ListItemLink } from "./GatsbyLinkWrappers"
 import { Song } from "../types/song"
 import generateSongDescription from "../utils/generateSongDescription"
 
-interface SongListItemProps extends Song {
+interface SongListItemProps {
   bookId: string
   bookTitle: string
+  song: Song
+  style: React.CSSProperties
 }
 
-const SongListItem: React.FunctionComponent<SongListItemProps> = ({
-  title,
-  type,
-  page,
-  id,
-  bookId,
-  bookTitle,
-}) => (
-  <ListItemLink button to={`/${bookId}/${id}`}>
-    <ListItemText
-      primary={title}
-      secondary={generateSongDescription(type, bookTitle, page)}
-    />
-  </ListItemLink>
+const SongListItem: React.FunctionComponent<SongListItemProps> = React.memo(
+  ({ bookId, bookTitle, song, style }) => {
+    const { title, type, page, id } = song
+
+    return (
+      <ListItemLink button to={`/${bookId}/${id}`} style={style}>
+        <ListItemText
+          primary={title}
+          secondary={generateSongDescription(type, bookTitle, page)}
+        />
+      </ListItemLink>
+    )
+  }
 )
 
 export default SongListItem
